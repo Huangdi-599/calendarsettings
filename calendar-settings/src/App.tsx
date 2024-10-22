87a4bacc9bc69f566773e61d9e0124da3010e9b7
Muideen27 <ilorimuideen0000@gmail.com>
2024-10-22 17:22:38 +0100

    leads routing

--- calendar-settings/src/App.tsx
+++ calendar-settings/src/App.tsx
@@ -3,6 +3,7 @@ import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Calendar from './components/Calendar'; 
 import LeadPage from './components/LeadPage';
+import Leads from './components/Leads';
 
 // Define the primary color
 const theme = createTheme({
@@ -23,6 +24,8 @@ function App() {
           <Route path="/" element={<Calendar />} />
           {/* LeadPage component */}
           <Route path="/leadpage" element={<LeadPage />} />
+          {/* Leads page */}
+          <Route path="/leads" element={<Leads />} />
         </Routes>
       </Router>
     </ThemeProvider>
