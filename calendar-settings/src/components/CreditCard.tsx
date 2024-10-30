6f1cf2014191322e8265550697323e078b45699b
Muideen27 <ilorimuideen0000@gmail.com>
2024-10-30 07:14:47 +0100

    back card styling cardType and band fixed.

--- calendar-settings/src/components/CreditCard.tsx
+++ calendar-settings/src/components/CreditCard.tsx
@@ -81,16 +81,16 @@ const CreditCard: React.FC<CreditCardProps> = ({
 
         {/* Back Side of the Card */}
         <div className="card-item__side -back" style={{ backgroundImage: `url(${cardBackground})` }}>
-        <div className="card-item__band"></div>
-
-        {/* CVV Label positioned above the CVV field */}
-        <div className="card-item__cvv-container">
-            <span className="card-item__cvv-label">CVV</span>
-            <span className="card-item__cvv">{cvv ? '***' : ''}</span> {/* Show '***' only if cvv has a value */}
-        </div>
-
-        {/* Card Type Logo at the bottom left */}
-        <img src={cardType} alt="Card Type" className="card-item__type-back" />
+            <div className="card-item__band"></div>
+            
+            <div className="card-item__cvv-container">
+                <span className="card-item__cvv">
+                {cvv ? '***' : ''}
+                </span>
+                <span className="card-item__cvv-label">CVV</span>
+            </div>
+            
+            <img src={cardType} alt="Card Type" className="card-item__type-back" />
         </div>
 
 
