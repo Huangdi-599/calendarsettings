b893d74590ef5c0353f49d78c1cd1782ff68e2e7
sanimuhammad09 <muhammadzenqeeth@gmail.com>
2024-11-04 15:16:06 +0100

    update

--- calendar-settings/src/App.tsx
+++ calendar-settings/src/App.tsx
@@ -7,6 +7,10 @@ import Dashboard from './components/Dashboard';
 import DashboardLayout from './components/DashboardLayout';
 import PurchasedLeads from './components/PurchasedLeads';
 import Feedback from './components/Feedback';
+import SignUpForm from './components/SignUpForm';
+import LoginForm from './components/LoginForm';
+import React from 'react';
+import PaymentSuccess from './components/PaymentSuccess';
 
 
 // Define your custom theme with the primary color
@@ -19,18 +23,20 @@ const theme = createTheme({
 });
 
 function App() {
+  const [showSignUp, setShowSignUp] = React.useState(false);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline /> {/* Resets CSS defaults */}
       <Calendar />
       <LeadList/>
       <Dashboard/>
-      < DashboardLayout/>
-      < PurchasedLeads/>
+      <DashboardLayout/>
+      <PurchasedLeads/>
       <Feedback/>
+      <PaymentSuccess/>
     </ThemeProvider>
   );
 }
-
 export default App;
 
